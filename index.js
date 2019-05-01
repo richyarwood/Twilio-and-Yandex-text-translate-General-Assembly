891686b0a135feeccde322e3fc1f89592e5949f8
Richard Yarwood <richyarwood@gmail.com>
2019-05-01 14:25:15 +0100

    Added in language and comments

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-require('dotenv').config()
-const rp = require('request-promise')
+require('dotenv').config() // Imports the .env file but is not sent to github
 
-const twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+const express = require('express') // This provides framework for our API
+const bodyParser = require('body-parser') // Make req.body
+const rp = require('request-promise') // Request promise for server-side request to twilio
+const twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN) // Twilio authentication
 
-// twilio.messages.create({
-//   body: 'Hello from RICHARD AND CHARLOTTE',
-//   to: '+447887626453',  // Text this number
-//   from: '+447447988117' // From a valid Twilio number
-// })
-//   .then((message) => console.log(message.sid))
+const app = express() // Loads express
 
-rp({
-  method: 'POST',
-  url: `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${process.env.YANDEX_API_KEY}&text=hello&lang=en-fr`, // `process` means this current app.
-  json: true
-})
-  .then(res => {
-    twilio.messages.create({
-      body: res.text,
-      to: '+447887626453',  // Text this number
-      from: '+447447988117' // From a valid Twilio number
-    })
-      .then((message) => console.log(message.sid))
+app.get('/', (req, res) => res.json({ message: 'You are on the Twilio translater api'}))
+
+app.use(bodyParser.json()) //Middleware which parses request to JSON
+
+function message(req, res){
+
+  // This is the POST function which sends a translation request to Yandex
+  rp({
+    method: 'POST',
+    url: `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${process.env.YANDEX_API_KEY}&text=${req.body.text}&lang=${req.body.lang}`, // `process` means this current app.
+    json: true
   })
+
+  // Once we have a response from Yandex, Twilio message.create is called and uses the response.text from Yandex as the message body. Named 'response' to differentiate from the initial message 'res' above
+
+    .then(response => { //This is the Yandex response
+      return twilio.messages.create({
+        body: response.text, //This is translation from Yandex
+        to: req.body.tel,  // This is from the original API POST request
+        from: '+447447988117' // Hard-coded Twilio trial number
+      })
+    })
+
+    .then(() => res.json({ message: 'Translation successful, message sent' }))
+}
+
+app.post('/message', message)
+
+app.listen(4000, () => console.log('We are listening on port 4000'))
