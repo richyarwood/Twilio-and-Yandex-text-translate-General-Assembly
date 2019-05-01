6c36295427569936dbcb03c1440b86e21832665b
Richard Yarwood <richyarwood@gmail.com>
2019-05-01 11:45:51 +0100

    Yandex api set up

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
-// const rp = require('request-promise')
+const rp = require('request-promise')
 
-const twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+// const twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+//
+// // twilio.messages.create({
+// //   body: 'Hello from RICHARD AND CHARLOTTE',
+// //   to: '+447887626453',  // Text this number
+// //   from: '+447447988117' // From a valid Twilio number
+// // })
+// //   .then((message) => console.log(message.sid))
 
-twilio.messages.create({
-  body: 'Hello from RICHARD AND CHARLOTTE',
-  to: '+447887626453',  // Text this number
-  from: '+447447988117' // From a valid Twilio number
+rp({
+  method: 'POST',
+  url: `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${process.env.YANDEX_API_KEY}&text=hello&lang=en-fr`, // `process` means this current app.
+  json: true
 })
-  .then((message) => console.log(message.sid))
-
-// rp({
-//   method: 'GET',
-//   url: `https://restcountries.eu/rest/v2/all/${process.env.DARKSKY_SECRET_KEY}`, // `process` means this current app.
-//   json: true
-// })
-//   .then(res => console.log(res))
+  .then(res => console.log(res))
